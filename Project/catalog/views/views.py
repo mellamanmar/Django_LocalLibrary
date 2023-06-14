d7b76040601ca932013c4bc51751dc377056db58
mellamanmar <marimelissamc@gmail.com>
2023-06-14 15:31:32 -0500

    Add templaates

--- Project/catalog/views/views.py
+++ Project/catalog/views/views.py
@@ -4,18 +4,20 @@ from django.shortcuts import render
 
 from ..models.author import Author
 from ..models.genre import  Genre
+from ..models.book import Book
+from ..models.book_instance import BookInstance
+from ..models.language import Language
 
 def index(request):
-    """Function to home site"""
-    authors= Author.objects.all()
-    genres= Genre.objects.all()
+    num_books=Book.objects.all().count()
+    num_instances=BookInstance.objects.all().count()
+    # Libros disponibles (status = 'a')
+    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+    num_authors=Author.objects.count()
 
     return render(
         request,
         'index.html',
-        context=
-        {
-            "authors":authors,
-            "genres":genres
-        }
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
     )
+
